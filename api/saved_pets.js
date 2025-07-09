aece3381de840fe14f682e5f219bc1458044005e
ChanellVS <zariachanellb@gmail.com>
2025-07-09 19:11:57 -0400

    Saved pets API js file deleted; API calls for saved pets added to users and pets API js files.

--- api/saved_pets.js
+++
@@ -1,8 +0,0 @@
-import { verifyToken } from "../middleware/auth.js";
-import express from "express";
-import { savePets, getSavedPetByUserId, deleteSavedPet } from "../db/queries/saved_pets.js";
-
-const router = express.Router();
-
-// Save a pet
-router.post
