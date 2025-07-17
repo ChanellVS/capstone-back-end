a0e9a547721848ddda0a5c05ee2fd8b6b8af55eb
Claudia <cludama83@gmail.com>
2025-07-17 03:53:26 -0400

    Applied s3 bucket, updated and installed packets to integrate the bucket.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import s3Routes from './api/s3.js';
 import messagesRouter from './api/messages.js';
 import userRouter from './api/users.js';
 import petsRouter from './api/pets.js';
@@ -22,6 +23,7 @@ app.use('/api/messages', messagesRouter);
 app.use('/api/users', userRouter);
 app.use('/api/pets', petsRouter);
 app.use('/api/geocode', geocodeRouter);
+app.use('/api/s3', s3Routes); 
 
 // Error handler
 app.use((err, req, res, next) => {
